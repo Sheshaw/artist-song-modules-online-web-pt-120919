4fefc7d4f370ef084ab37b5046d013f0bc59a15b
Chayan Martinez <nayachmartinez68@gmail.com>
2020-01-31 00:58:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,11 @@
 require 'pry'
+require_relative '../config/environment'
+
 
 class Artist
+  extend Memorable
+  include Paramable
+
   attr_accessor :name
   attr_reader :songs
 
@@ -10,6 +15,7 @@ class Artist
     @@artists << self
     @songs = []
   end
+ 
 
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
@@ -17,14 +23,6 @@ class Artist
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -36,7 +34,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+    #name.downcase.gsub(' ', '-')
+  #end
 end
